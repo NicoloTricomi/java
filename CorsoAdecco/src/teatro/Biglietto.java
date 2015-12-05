210bf1ad20c4fa4e09a7774652561359475a7f16
Nico <Nico@192.168.2.139>
2015-12-05 12:56:33 +0100

    modificato getPosto()

---
+++ CorsoAdecco/src/teatro/Biglietto.java
@@ -0,0 +1,26 @@
+package teatro;
+
+public class Biglietto 
+{
+	private Posto posto;
+	private Utente utente;
+
+	public Biglietto(Posto posto, Utente utente)
+	{
+		this.posto = posto;
+		this.utente = utente;
+	}
+
+	public Posto getPosto()
+	{
+		System.out.println("posto ritornato");
+		return posto;
+	}
+
+	public Utente getUtente()
+	{
+		return utente;
+	}
+	
+
+}
